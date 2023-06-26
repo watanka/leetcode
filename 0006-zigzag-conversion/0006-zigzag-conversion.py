deb0513f5d69865b4cac0579fb03283b3e23c2d7
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-06-26 17:12:30 +0900

    Time: 69 ms (77.42%), Space: 16.3 MB (84.12%) - LeetHub

--- 0006-zigzag-conversion/0006-zigzag-conversion.py
+++ 0006-zigzag-conversion/0006-zigzag-conversion.py
@@ -4,40 +4,22 @@ class Solution:
         if numRows == 1 :
             return s
 
-        i = 0
-        result = ''
+        freq = numRows + numRows - 2
 
-        numIter = numRows + numRows - 2 
+        num_segs = len(s) // freq + (1 if len(s) % freq else 0)
 
-        while i < len(s) :
+        segments = [ s[freq * i :  min(len(s), freq * (i + 1))] \
+                                for i in range(num_segs)]
 
-            trg_idx = i % numIter
+        zigzag = [''] * numRows
 
-            if trg_idx % numIter < numRows :
-                result += s[i]
-                i += 1
+        for seg in segments :
+            for i in range(len(seg)) : 
+                
+                if i % freq < numRows :
+                    zigzag[i] += seg[i]
+                else :
+                    zigzag[freq - i] += seg[i]
 
-            else : 
-                for numSpace in range(numRows - 2, 0, -1) :
-                    result += ' ' * numSpace + s[i] + ' ' * (numRows - numSpace - 1)
-                    i += 1
-                    if i >= len(s) :
-                        break
-
-        numCols = (len(result) // numRows) + (1 if len(result) % numRows else 0)
-        
-        str_list = [[''] * numCols for _ in range(numRows)]
-
-        idx = 0
-        for i in range(numCols) : 
-            for j in range(numRows) :
-                if idx >= len(result) :
-                    break
-                if result[idx] != ' ' :
-                    str_list[j][i] = result[idx] 
-                idx += 1
-            if idx >= len(result) :
-                    break
-
-        return ''.join([''.join(strvalue) for strvalue in str_list])
-
+        return ''.join(zigzag)
+                
