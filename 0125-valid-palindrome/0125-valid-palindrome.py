8871a942a70fde62c804b875ef7e2835f445043a
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-06-28 12:14:20 +0900

    Time: 56 ms (84.72%), Space: 22.6 MB (5.22%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.py
+++ 0125-valid-palindrome/0125-valid-palindrome.py
@@ -6,17 +6,5 @@ class Solution:
         allowed = 'abcdefghijklmnopqrstuvwxyz0123456789'
         s = [letter.lower() for letter in s if letter.lower() in allowed]
 
-        num_iter = len(s) // 2
-
-        start, end = 0, len(s) - 1
-
-        for _ in range(num_iter) :
-            if s[start] != s[end] :
-                return False
-            else :
-                start += 1
-                end -= 1
-
-        return True
-
+        return s == s[::-1]
 
