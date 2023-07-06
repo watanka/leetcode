750c1183c52c437dfc8f61f265e6b047170680c4
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-06 16:17:43 +0900

    Time: 79 ms (95.96%), Space: 16.7 MB (18.32%) - LeetHub

--- 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
+++ 0030-substring-with-concatenation-of-all-words/0030-substring-with-concatenation-of-all-words.py
@@ -1,29 +1,34 @@
 class Solution:
     def findSubstring(self, s: str, words: List[str]) -> List[int]:
         count = {}
-        for w in words:
-            count[w] = count.get(w, 0) + 1
+        for word in words :
+            count[word] = count.get(word, 0) + 1
+
         
-        lw = len(words[0])
-        fin = []
 
-        for i in range(lw):
+        wordLength = len(words[0])
+        
+        answer = []
+
+        for i in range(wordLength) :
             l = i
-            window_hash = {}
+            currMap = {}
 
-            for r in range(i, len(s) - lw + 1, lw):
-                word = s[r:r + lw]
-                if word not in count:
-                    window_hash.clear()
-                    l = r + lw
-                else:
-                    window_hash[word] = window_hash.get(word, 0) + 1
+            for r in range(i, len(s) - wordLength + 1, wordLength) :
+                word = s[r : r + wordLength]
 
-                    while window_hash[word] > count[word]:
-                        window_hash[s[l:l + lw]] -= 1
-                        l += lw
+                if word not in count :
+                    currMap.clear()
+                    l = r + wordLength
 
-                    if window_hash == count:
-                        fin.append(l)
+                else :
+                    currMap[word] = currMap.get(word, 0) + 1
+                    while currMap[word] > count[word] :
+                        currMap[s[l : l + wordLength]] -= 1
+                        l += wordLength
+                    
+                    if currMap == count :
+                        answer.append(l)
+        return answer
 
-        return fin
+    
