6c420f050ae3a8675c5eb5b9f3168cbe78aa374e
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-07 17:10:23 +0900

    Time: 136 ms (76.67%), Space: 17.2 MB (65.06%) - LeetHub

--- 0076-minimum-window-substring/0076-minimum-window-substring.py
+++ 0076-minimum-window-substring/0076-minimum-window-substring.py
@@ -1,58 +1,53 @@
+from collections import defaultdict, Counter
+
 class Solution:
-    def minWindow(self, s : str, t: str) -> str :
-        answer = ''
+    def minWindow(self, s: str, t: str) -> str:
+        window = defaultdict(int)
+        dict_t = Counter(t)
+        needed = len(dict_t)
+        found = 0
+        l = 0
+        result = float("inf"), None, None
+        for r in range(len(s)):
+            char = s[r]
+            window[char] += 1
+            
+            if char in dict_t and window[char] == dict_t[char]:
+                found += 1
 
-        # t에 있는 글자들과 갯수를 dictionary로 저장한다.
-        tCount = {}
-        for char in t :
-            tCount[char] = tCount.get(char, 0) + 1
+            while l <= r and found == needed:
+                char = s[l]
+
+                if r - l + 1 < result[0]:
+                    result = (r - l + 1, l, r)
+
+                window[char] -= 1
+                if char in dict_t and window[char] < dict_t[char]:
+                    found -= 1
+                
+                l += 1
         
-        # sCount에 substring에 채워야하는 글자들을 채워넣는다.
-        sCount = {}
-        for key, cnt in tCount.items() :
-            sCount[key] = 0
-
-        def test() :
-            for key, val in tCount.items() :
-                if val > sCount[key] :
-                    return False
-            return True
-
-        left, right = 0, 0
-
-        # 처음 보는 t 글자의 index로 left를 업데이트해준다.
-        for i in range(len(s)) :
-            if s[i] in sCount.keys() :
-                left = i
-                break
-        
-        for i in range(left, len(s)) :
-
-            right = i
-            if s[i] in tCount.keys() :
-                sCount[s[i]] += 1
-                while test() : # tCount의 값 중 하나라도 모자랄 경우, window substring 조건을 만족하지 못한 것
-                    substring = s[left: right + 1]
-                    if answer == '' or len(substring) < len(answer) :
-                        answer = substring
-
-                    # left 인덱스를 이동
-                    sCount[s[left]] -= 1
-                    for j in range(left+1, right+1) :
-                        if s[j] in tCount.keys() :
-                            left = j
-                            break
-        
-        if test() :
-            if answer == '' or len(substring) < len(answer) :
-                answer = substring
-        return answer
+        length, l, r = result
+        return "" if length == float("inf") else s[l:r+1]
 
 
-
-
-
-
+        # ADOBECODEBANC ABC
+        # dict_t = {A : 1, B: 2, C: 3}
+        # needed = 3
+        # found = 0
+        # r : 0 l : 0 char: A window={A:1} found = 1  result (inf, 0, 0)
+        # r : 1 l : 0 char: D window={A:1 D:1} found = 1  result (inf, 0, 0)
+        # r : 2 l : 0 char: O window={A:1 D:1 O:1} found = 1  result (inf, 0, 0)
+        # r : 3 l : 0 char: B window={A:1 D:1 O:1 B:1} found = 2  result (inf, 0, 0)
+        # r : 4 l : 0 char: E window={A:1 D:1 O:1 B:1 E:1} found = 2  result (inf, 0, 0)
+        # r : 5 l : 0 char: C window={A:1 D:1 O:1 B:1 E:1 C:1} found = 3  => char: A result (6, 0, 5) l : 1 found = 2
+        # r : 6 l : 1 char: O window={A:0 D:1 O:2 B:1 E:1 C:1} found = 2 
+        # r : 7 l : 1 char: D window={A:0 D:2 O:2 B:1 E:1 C:1} found = 2 
+        # r : 8 l : 1 char: E window={A:0 D:2 O:2 B:1 E:2 C:1} found = 2 
+        # r : 9 l : 1 char: B window={A:0 D:2 O:2 B:2 E:2 C:1} found = 2 
+        # r : 10 l : 1 char: A window={A:1 D:2 O:2 B:2 E:2 C:1} found = 3 => char: C result (6, 5, 10) l : 6 found = 2
+        # r : 11 l : 6 char: N window={A:1 D:1 O:1 B:1 E:1 C:0 N:1} found = 2
+        # r : 12 l : 6 char: C window={A:1 D:1 O:1 B:1 E:1 C:1 N:1} found = 3 => char:O result(4, 9, 10) l : 10 found = 2
 
 
 
