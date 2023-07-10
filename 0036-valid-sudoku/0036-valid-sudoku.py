48d0834db92f4c5b22d37008e19d02df598699da
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-10 20:49:10 +0900

    Time: 121 ms (39.76%), Space: 16.3 MB (50.49%) - LeetHub

--- 0036-valid-sudoku/0036-valid-sudoku.py
+++ 0036-valid-sudoku/0036-valid-sudoku.py
@@ -4,24 +4,24 @@ class Solution:
 
         # validate row-wise
         for col in range(9) : 
-            num_set = [str(i) for i in range(1, 10)]
+            num_set = [0 for _ in range(10)]
             for row in range(9) :
-                if board[col][row] in num_set :
-                    num_set.remove(board[col][row])
-                elif board[col][row] == '.' :
+                if board[col][row] == '.' :
                     continue
+                elif not num_set[int(board[col][row])] :
+                    num_set[int(board[col][row])] = 1
                 else :
                     # print('row', board[col][row])
                     return False
 
         # validate column-wise
         for row in range(9) : 
-            num_set = [str(i) for i in range(1, 10)]
+            num_set = [0 for _ in range(10)]
             for col in range(9) :
-                if board[col][row] in num_set :
-                    num_set.remove(board[col][row])
-                elif board[col][row] == '.' :
+                if board[col][row] == '.' :
                     continue
+                elif not num_set[int(board[col][row])] :
+                    num_set[int(board[col][row])] = 1
                 else :
                     # print('col', board[col][row])
                     return False
@@ -29,14 +29,14 @@ class Solution:
         # validate 3x3 sub-boxes
         for blck_row in range(3) :
             for blck_col in range(3) :
-                num_set = [str(i) for i in range(1, 10)]
+                num_set = [0 for _ in range(10)]
                 for j in range(3) :
                     for k in range(3) :
                         
-                        if board[blck_row * 3 + j][blck_col * 3 + k] in num_set :
-                            num_set.remove(board[blck_row * 3 + j][blck_col * 3 + k])
-                        elif board[blck_row * 3 + j][blck_col * 3 + k] == '.' :
+                        if board[blck_row * 3 + j][blck_col * 3 + k] == '.' :
                             continue
+                        elif not num_set[int(board[blck_row * 3 + j][blck_col * 3 + k])]:
+                            num_set[int(board[blck_row * 3 + j][blck_col * 3 + k])] = 1
                         else :
                             # print('3x3', board[blck_row * 3 + j][blck_col * 3 + k])
                             return False
