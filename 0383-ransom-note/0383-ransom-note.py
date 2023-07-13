7cbc63070b1e45a7a2a209caf7348e12b6a9ba46
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-13 17:02:18 +0900

    Time: 87 ms (37.99%), Space: 16.4 MB (92.88%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,12 +1,12 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
 
-        checked_letters = set()
-        for char in ransomNote :
-            if char not in checked_letters and ransomNote.count(char) > magazine.count(char) :
-                return False
-            checked_letters.add(char)
-        return True
+        # checked_letters = set()
+        # for char in ransomNote :
+        #     if char not in checked_letters and ransomNote.count(char) > magazine.count(char) :
+        #         return False
+        #     checked_letters.add(char)
+        # return True
 
         
         
@@ -15,21 +15,27 @@ class Solution:
         
         
         # 2nd approach : get dictionary of magazine, and loop through ransomNote
-        # magazine_cnt = {}
-        # for char in magazine :
-        #     magazine_cnt[char] = magazine_cnt.get(char, 0) + 1
+        magazine_cnt = {}
+        for char in magazine :
+            magazine_cnt[char] = magazine_cnt.get(char, 0) + 1
+
+        checked_letters = set()
+        for char in ransomNote :
+            if char in magazine_cnt :
+                if char not in checked_letters :
+                    if magazine_cnt[char] < ransomNote.count(char) :
+                        return False
 
 
-        # for char in ransomNote :
-        #     if char in magazine_cnt :
-        #         magazine_cnt[char] -= 1
+                # magazine_cnt[char] -= 1
 
-        #         if magazine_cnt[char] < 0 :
-        #             return False
-        #     else :
-        #         return False
+                # if magazine_cnt[char] < 0 :
+                #     return False
+            else :
+                return False
+            checked_letters.add(char)
 
-        # return True
+        return True
         
         
         
