fb2229a47e1b5d9ccdf8adc0a0237070b962d778
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-13 16:44:26 +0900

    Time: 88 ms (35.84%), Space: 16.6 MB (65.82%) - LeetHub

--- 0383-ransom-note/0383-ransom-note.py
+++ 0383-ransom-note/0383-ransom-note.py
@@ -1,20 +1,43 @@
 class Solution:
     def canConstruct(self, ransomNote: str, magazine: str) -> bool:
-        ransom_cnt = {}
-        for char in ransomNote :
-            ransom_cnt[char] = ransom_cnt.get(char, 0) + 1
-
+        # 2nd approach
         magazine_cnt = {}
         for char in magazine :
             magazine_cnt[char] = magazine_cnt.get(char, 0) + 1
 
-        for char in ransom_cnt.keys() :
+
+        for char in ransomNote :
             if char in magazine_cnt :
-                if magazine_cnt[char] < ransom_cnt[char] :
+                magazine_cnt[char] -= 1
+
+                if magazine_cnt[char] < 0 :
                     return False
+
             else :
                 return False
+
         return True
+        
+        
+        
+        
+        
+        # 1st approach : get dictionaries of both strings 
+        # ransom_cnt = {}
+        # for char in ransomNote :
+        #     ransom_cnt[char] = ransom_cnt.get(char, 0) + 1
+
+        # magazine_cnt = {}
+        # for char in magazine :
+        #     magazine_cnt[char] = magazine_cnt.get(char, 0) + 1
+
+        # for char in ransom_cnt.keys() :
+        #     if char in magazine_cnt :
+        #         if magazine_cnt[char] < ransom_cnt[char] :
+        #             return False
+        #     else :
+        #         return False
+        # return True
 
 
 
