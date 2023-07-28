f0566317835e4233009cd0faf55c0115e800f82c
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-28 15:15:18 +0900

    Time: 58 ms (99.28%), Space: 20.1 MB (91.11%) - LeetHub

--- 0155-min-stack/0155-min-stack.py
+++ 0155-min-stack/0155-min-stack.py
@@ -2,19 +2,46 @@ class MinStack:
 
     def __init__(self):
         self.stack = []
-        
+        self.minValue = 2**31 - 1
 
     def push(self, val: int) -> None:
         self.stack.append(val)
+        if self.minValue > val :
+            self.minValue = val
 
     def pop(self) -> None:
-        self.stack.pop(-1)
+
+        popVal = self.stack.pop(-1)
+        if self.minValue == popVal :
+            if self.stack :
+                self.minValue = min(self.stack)
+            else :
+                self.minValue = 2**31 - 1
+        
 
     def top(self) -> int:
         return self.stack[-1]
 
     def getMin(self) -> int:
-        return min(self.stack)
+        return self.minValue
+
+
+    ## 1st approach. use list
+    # def __init__(self):
+    #     self.stack = []
+        
+
+    # def push(self, val: int) -> None:
+    #     self.stack.append(val)
+
+    # def pop(self) -> None:
+    #     self.stack.pop(-1)
+
+    # def top(self) -> int:
+    #     return self.stack[-1]
+
+    # def getMin(self) -> int:
+    #     return min(self.stack)
         
 
 
