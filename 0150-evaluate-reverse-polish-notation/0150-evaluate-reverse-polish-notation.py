f18cf579a0f01bbf95fa82d0ae0e83451eae557b
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-07-29 16:09:21 +0900

    Time: 135 ms (6.51%), Space: 16.7 MB (87.14%) - LeetHub

--- 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
+++ 0150-evaluate-reverse-polish-notation/0150-evaluate-reverse-polish-notation.py
@@ -1,11 +1,11 @@
 class Solution:
     def evalRPN(self, tokens: List[str]) -> int:
-
-        numstack = []
-
-        answer = None
+        
+        
         if len(tokens) == 1 :
             return int(tokens[0])
+        
+        numstack = []
 
         for token in tokens :
             if token.replace('-', '').isnumeric() :
