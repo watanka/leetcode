58da14b714746469e7e5df78639f941670c6d5c3
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-02 15:26:19 +0900

    Time: 51 ms (63.92%), Space: 16.4 MB (54.06%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,31 +5,50 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
-        
-        if not (l1 or l2) :
-            return None
-        
+
         answer = ListNode()
         tail = answer
 
-        while l1 or l2 :
-
-            if l1 and not l2 :
+        while l1 and l2 :
+            if l1.val <= l2.val :
                 tail.next = ListNode(l1.val)
                 l1 = l1.next
-            elif l2 and not l1 :
+            else :
                 tail.next = ListNode(l2.val)
                 l2 = l2.next
-
-            elif l1.val <= l2.val :
-                tail.next = ListNode(l1.val)
-                l1 = l1.next
-            elif l1.val > l2.val :
-                tail.next = ListNode(l2.val)
-                l2 = l2.next
-
             tail = tail.next
+        # 두 리스트 중 남은 리스트는 answer 뒤에 그대로 붙인다.
+        if l1 or l2 :
+            tail.next = l1 if l1 else l2
+        
         return answer.next
+        
+
+        # 2nd.approach
+        # if not (l1 or l2) :
+        #     return None
+        
+        # answer = ListNode()
+        # tail = answer
+
+        # while l1 or l2 :
+
+        #     if l1 and not l2 :
+        #         tail.next = ListNode(l1.val)
+        #         l1 = l1.next
+        #     elif l2 and not l1 :
+        #         tail.next = ListNode(l2.val)
+        #         l2 = l2.next
+
+        #     elif l1.val <= l2.val :
+        #         tail.next = ListNode(l1.val)
+        #         l1 = l1.next
+        #     elif l1.val > l2.val :
+        #         tail.next = ListNode(l2.val)
+        #         l2 = l2.next
+
+        #     tail = tail.next
+        # return answer.next
         
         # 1st.approach
         # if not (l1 or l2) :
