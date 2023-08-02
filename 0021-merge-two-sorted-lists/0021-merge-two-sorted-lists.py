561d37da5444d74456f0cd21f0c1fcb9d34fdcdb
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-02 15:23:32 +0900

    Time: 42 ms (94.11%), Space: 16.2 MB (82.92%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.py
@@ -5,32 +5,59 @@
 #         self.next = next
 class Solution:
     def mergeTwoLists(self, l1: Optional[ListNode], l2: Optional[ListNode]) -> Optional[ListNode]:
+        
         if not (l1 or l2) :
             return None
+        
         answer = ListNode()
         tail = answer
-    
+
         while l1 or l2 :
 
-            if not l1 :
-                tail.val = l2.val
-                l2 = l2.next
-            elif not l2 :
-                tail.val = l1.val
+            if l1 and not l2 :
+                tail.next = ListNode(l1.val)
                 l1 = l1.next
-            else :
-                if l1.val <= l2.val :
-                    tail.val = l1.val
-                    l1 = l1.next
-                elif l1.val > l2.val :
-                    tail.val = l2.val
-                    l2 = l2.next
-            if l1 or l2 :
-                tail.next = ListNode()
-                tail = tail.next
+            elif l2 and not l1 :
+                tail.next = ListNode(l2.val)
+                l2 = l2.next
+
+            elif l1.val <= l2.val :
+                tail.next = ListNode(l1.val)
+                l1 = l1.next
+            elif l1.val > l2.val :
+                tail.next = ListNode(l2.val)
+                l2 = l2.next
+
+            tail = tail.next
+        return answer.next
+        
+        # 1st.approach
+        # if not (l1 or l2) :
+        #     return None
+        # answer = ListNode()
+        # tail = answer
+    
+        # while l1 or l2 :
+
+        #     if not l1 :
+        #         tail.val = l2.val
+        #         l2 = l2.next
+        #     elif not l2 :
+        #         tail.val = l1.val
+        #         l1 = l1.next
+        #     else :
+        #         if l1.val <= l2.val :
+        #             tail.val = l1.val
+        #             l1 = l1.next
+        #         elif l1.val > l2.val :
+        #             tail.val = l2.val
+        #             l2 = l2.next
+        #     if l1 or l2 :
+        #         tail.next = ListNode()
+        #         tail = tail.next
             
         
         
         
 
-        return answer
+        # return answer
