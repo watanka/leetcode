261a51da9e62f1346166101e1d84ad347eac963d
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-07 16:38:57 +0900

    Time: 38 ms (98.48%), Space: 16.5 MB (18.19%) - LeetHub

--- 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
+++ 0082-remove-duplicates-from-sorted-list-ii/0082-remove-duplicates-from-sorted-list-ii.py
@@ -5,19 +5,36 @@
 #         self.next = next
 class Solution:
     def deleteDuplicates(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        if head is None : return head
+
         dummy = ListNode(next = head)
-        curr = dummy
+        prev, curr = dummy, head.next
 
         while curr :
-            if (curr.next and curr.next.next) and curr.next.val == curr.next.next.val :
-                tmp = curr.next.next
-                while tmp and tmp.next and tmp.val ==  tmp.next.val :
-                    tmp = tmp.next
-                curr.next = tmp.next
+            # 만약 포인터의 앞 두 값이 같다면
+            if prev.next.val == curr.val :
+                while curr and prev.next.val == curr.val :
+                    curr = curr.next
+                prev.next = curr
+                if curr :
+                    curr = curr.next
             else :
-                curr = curr.next
+                prev, curr = prev.next, curr.next
 
-
-
-            
         return dummy.next
+        
+        
+        ## 1st.approach
+        # dummy = ListNode(next = head)
+        # curr = dummy
+
+        # while curr :
+        #     if (curr.next and curr.next.next) and curr.next.val == curr.next.next.val :
+        #         tmp = curr.next.next
+        #         while tmp and tmp.next and tmp.val ==  tmp.next.val :
+        #             tmp = tmp.next
+        #         curr.next = tmp.next
+        #     else :
+        #         curr = curr.next
+    
+        # return dummy.next
