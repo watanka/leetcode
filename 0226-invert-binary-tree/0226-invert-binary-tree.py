c4baf420cfe337e9b3cff081d2cadaad86004b5c
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-15 16:29:45 +0900

    Time: 38 ms (84.77%), Space: 16.2 MB (93.94%) - LeetHub

--- 0226-invert-binary-tree/0226-invert-binary-tree.py
+++ 0226-invert-binary-tree/0226-invert-binary-tree.py
@@ -8,20 +8,30 @@
 from collections import deque
 class Solution:
     def invertTree(self, root: Optional[TreeNode]) -> Optional[TreeNode]:
+        
+        # 2nd. recursive
+        if not root :
+            return None
+        def recursive(node) :
+            if node :
+                node.left, node.right = node.right, node.left
+                recursive(node.left)
+                recursive(node.right)
+            return node
 
-        pt = root
-        queue = deque([pt])
+        return recursive(root)
 
-        while queue :
-            for _ in range(len(queue)) :
-                node = queue.popleft()
-                if node :
-                    # if node.left and node.right :
-                        # node.left, node.right = node.right, node.left
-                    # elif node.left or node.right :
-                    node.left, node.right = node.right, node.left
-                    queue.append(node.left)
-                    queue.append(node.right)
+        # # 1st bfs
+        # pt = root
+        # queue = deque([pt])
+
+        # while queue :
+        #     for _ in range(len(queue)) :
+        #         node = queue.popleft()
+        #         if node :
+        #             node.left, node.right = node.right, node.left
+        #             queue.append(node.left)
+        #             queue.append(node.right)
                     
-        return root
+        # return root
                 
