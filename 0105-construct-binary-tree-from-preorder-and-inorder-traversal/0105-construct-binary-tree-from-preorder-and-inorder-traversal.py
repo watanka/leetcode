50978f822402baccf6736c657b9549123a7d1384
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-17 15:46:53 +0900

    Time: 381 ms (5.48%), Space: 383.1 MB (5.27%) - LeetHub

--- 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
+++ 0105-construct-binary-tree-from-preorder-and-inorder-traversal/0105-construct-binary-tree-from-preorder-and-inorder-traversal.py
@@ -7,15 +7,22 @@
 from collections import deque
 
 class Solution:
-    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
-        if not preorder or not inorder :
-            return None
-        
-        root = TreeNode(preorder[0])
-        mid = inorder.index(preorder[0])
-        root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
-        root.right = self.buildTree(preorder[mid + 1:], inorder[mid+1:])
 
-        return root
+    def buildTree(self, preorder: List[int], inorder: List[int]) -> Optional[TreeNode]:
+
+        hashmap = {num : i for i, num in enumerate(inorder)}
+
+        def _buildTree(preorder, inorder) :
+            if not preorder or not inorder :
+                return None
+            
+            root = TreeNode(preorder[0])
+            mid = hashmap[preorder[0]]
+            root.left = self.buildTree(preorder[1:mid + 1], inorder[:mid])
+            root.right = self.buildTree(preorder[mid + 1:], inorder[mid+1:])
+
+            return root
+        
+        return _buildTree(preorder, inorder)
 
 
