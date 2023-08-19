56c0517075b3b66afd7fee5b138d2d7837d38eb2
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-19 15:22:22 +0900

    Time: 53 ms (80.01%), Space: 17.8 MB (16.92%) - LeetHub

---
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -0,0 +1,34 @@
+"""
+# Definition for a Node.
+class Node:
+    def __init__(self, val: int = 0, left: 'Node' = None, right: 'Node' = None, next: 'Node' = None):
+        self.val = val
+        self.left = left
+        self.right = right
+        self.next = next
+"""
+from collections import deque
+
+class Solution:
+    def connect(self, root: 'Node') -> 'Node':
+        queue = deque([root])
+
+        while queue :
+            
+            len_q = len(queue) # initial length of queue is same as # of nodes in same level. connect # nodes-1 that are on same level
+            i = 1
+            for _ in range(len_q) :
+                
+                node = queue.popleft()
+                
+                if node :
+                    # print(queue[0].val if queue else None)
+                    node.next = queue[0] if i < len_q else None
+                    i += 1
+                    if node.left :
+                        queue.append(node.left)
+                    if node.right :
+                        queue.append(node.right)
+        
+        
+        return root
