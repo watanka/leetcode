0549f816058b20542364104f1feb71d634c2ea4a
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-19 15:41:18 +0900

    Time: 48 ms (91.9%), Space: 17.7 MB (58.27%) - LeetHub

--- 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
+++ 0117-populating-next-right-pointers-in-each-node-ii/0117-populating-next-right-pointers-in-each-node-ii.py
@@ -11,22 +11,30 @@ from collections import deque
 
 class Solution:
     def connect(self, root: 'Node') -> 'Node':
+        
+        # def helper(root) :
+        #     if not root :
+
+        #     root.left.next = helper(root.right
+
+        # 1st. approach
         queue = deque([root])
 
         while queue :
             
             len_q = len(queue) # initial length of queue is same as # of nodes in same level. connect # nodes-1 that are on same level
+            tmp = deque([])
             for i in range(len_q) :
-                
+
                 node = queue.popleft()
                 if node :
                     # print(queue[0].val if queue else None)
                     node.next = queue[0] if i < len_q - 1 else None # connect #(samelevel node) - 1
-                    i += 1
                     if node.left :
-                        queue.append(node.left)
+                        tmp.append(node.left)
                     if node.right :
-                        queue.append(node.right)
+                        tmp.append(node.right)
+            queue = tmp
         
         
         return root
