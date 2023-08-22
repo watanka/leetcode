01eb177cb38a7933979a4386d9c778023d2dddef
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-22 11:30:10 +0900

    Time: 42 ms (61.99%), Space: 16.4 MB (47.61%) - LeetHub

--- 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
+++ 0129-sum-root-to-leaf-numbers/0129-sum-root-to-leaf-numbers.py
@@ -6,31 +6,48 @@
 #         self.right = right
 class Solution:
     def sumNumbers(self, root: Optional[TreeNode]) -> int:
-        def dfs(stack, root_leaf_path) :
+        ## 2nd. recursive
+        result = 0
+        def helper(node, r_to_l_sum) :
             nonlocal result
-            while stack :
-                node = stack.pop()
-                root_leaf_path.append(node.val)
+            if not node :
+                return
+            if not node.left and not node.right :
+                # convert root to left path into a number
+                root2left_sum = int(''.join(map(str, r_to_l_sum + [node.val])))
+                result += root2left_sum
+            helper(node.left, r_to_l_sum + [node.val])
+            helper(node.right, r_to_l_sum + [node.val])
 
-                if not node.left and not node.right :
-                    root_leaf_sum = int(''.join(map(str, root_leaf_path)))
-                    result += root_leaf_sum
-                    return
+        helper(root, [])
+        return result
 
-                if node.left :
-                    dfs(stack + [node.left], root_leaf_path)
-                    root_leaf_path.pop()
-                if node.right :
-                    dfs(stack + [node.right], root_leaf_path)
-                    root_leaf_path.pop()
+        ## 1st. approach dfs
+        # def dfs(stack, root_leaf_path) :
+        #     nonlocal result
+        #     while stack :
+        #         node = stack.pop()
+        #         root_leaf_path.append(node.val)
+
+        #         if not node.left and not node.right :
+        #             root_leaf_sum = int(''.join(map(str, root_leaf_path)))
+        #             result += root_leaf_sum
+        #             return
+
+        #         if node.left :
+        #             dfs(stack + [node.left], root_leaf_path)
+        #             root_leaf_path.pop()
+        #         if node.right :
+        #             dfs(stack + [node.right], root_leaf_path)
+        #             root_leaf_path.pop()
                 
                 
         
-        result = 0        
-        stack = [root]
-        dfs(stack, root_leaf_path = [])
+        # result = 0        
+        # stack = [root]
+        # dfs(stack, root_leaf_path = [])
 
-        return result
+        # return result
                 
 
 
