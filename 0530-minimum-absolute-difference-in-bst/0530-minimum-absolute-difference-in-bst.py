fe0996fd2205d93d82039f57fe7db852535354c6
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-29 13:54:59 +0900

    Time: 51 ms (92.39%), Space: 18.5 MB (84.48%) - LeetHub

--- 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
+++ 0530-minimum-absolute-difference-in-bst/0530-minimum-absolute-difference-in-bst.py
@@ -10,22 +10,41 @@ import sys
 class Solution:
     def getMinimumDifference(self, root: Optional[TreeNode]) -> int:
 
-        self.minDistance = 1e9
-        # Initially, it will be null.
+        self.minDiff = 1e9
         self.prevNode = None
 
-        def inorder(node):
-            if node is None:
+        def inorder(node) :
+            if node is None :
                 return
+
             inorder(node.left)
-            # Find the difference with the previous value if it is there.
-            if self.prevNode is not None:
-                self.minDistance = min(self.minDistance, node.val - self.prevNode)
+            if self.prevNode is not None :
+                self.minDiff = min(self.minDiff, node.val - self.prevNode)
             self.prevNode = node.val
+
             inorder(node.right)
 
         inorder(root)
-        return self.minDistance
+
+        return self.minDiff
+
+
+        # self.minDistance = 1e9
+        # # Initially, it will be null.
+        # self.prevNode = None
+
+        # def inorder(node):
+        #     if node is None:
+        #         return
+        #     inorder(node.left)
+        #     # Find the difference with the previous value if it is there.
+        #     if self.prevNode is not None:
+        #         self.minDistance = min(self.minDistance, node.val - self.prevNode)
+        #     self.prevNode = node.val
+        #     inorder(node.right)
+
+        # inorder(root)
+        # return self.minDistance
 
 
         # # 1st approach. get ordered list and compare neighbors
