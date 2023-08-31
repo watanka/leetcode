2cbe20e7eb7bc0fbcc649c77ed87c698ea359d2f
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-08-31 13:22:59 +0900

    Time: 51 ms (62.31%), Space: 19 MB (31.15%) - LeetHub

--- 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
+++ 0098-validate-binary-search-tree/0098-validate-binary-search-tree.py
@@ -4,25 +4,36 @@
 #         self.val = val
 #         self.left = left
 #         self.right = right
+from collections import deque
 class Solution:
     def isValidBST(self, root: Optional[TreeNode]) -> bool:
+        # 2nd approach.
+        def check(node, low, high):
+            if not node: return True
+            if  node.val >= high or node.val <= low:
+                return False
+            return check(node.left, low, node.val) and check(node.right, node.val, high)
+        
+        return check(root, float('-inf'), float('inf'))
 
-        isValid = True 
-        prev = None
+        
+        # 1st approach.
+        # isValid = True 
+        # prev = None
 
-        def inorder(node) :
-            nonlocal prev, isValid
+        # def inorder(node) :
+        #     nonlocal prev, isValid
 
-            if not node :
-                return
-            inorder(node.left)
-            if prev is not None :
-                if node.val <= prev :
-                    isValid = False
-                    return
-            prev = node.val
-            inorder(node.right)
+        #     if not node :
+        #         return
+        #     inorder(node.left)
+        #     if prev is not None :
+        #         if node.val <= prev :
+        #             isValid = False
+        #             return
+        #     prev = node.val
+        #     inorder(node.right)
 
-        inorder(root)
+        # inorder(root)
 
-        return isValid
+        # return isValid
