03af23e9b9cef2c25d45b03b47e252180447a02b
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-10 21:42:59 +0900

    Time: 146 ms (78.29%), Space: 33.8 MB (64.92%) - LeetHub

--- 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
+++ 0208-implement-trie-prefix-tree/0208-implement-trie-prefix-tree.py
@@ -1,26 +1,67 @@
-class Trie:
+# class Trie:
 
-    def __init__(self):
-        self.ls = []
+#     def __init__(self):
+#         self.ls = []
         
 
-    def insert(self, word: str) -> None:
-        self.ls.append(word)
+#     def insert(self, word: str) -> None:
+#         self.ls.append(word)
         
 
-    def search(self, word: str) -> bool:
-        for search_ in self.ls :
-            if search_ == word :
-                return True
-        return False
+#     def search(self, word: str) -> bool:
+#         for search_ in self.ls :
+#             if search_ == word :
+#                 return True
+#         return False
         
 
-    def startsWith(self, prefix: str) -> bool:
-        for search_ in self.ls :
-            if search_.startswith(prefix) :
-                return True
-        return False
+#     def startsWith(self, prefix: str) -> bool:
+#         for search_ in self.ls :
+#             if search_.startswith(prefix) :
+#                 return True
+#         return False
+    
+class TrieNode :
+    def __init__(self) :
+        self.children = {}
+        self.endOfWord = False
+
+class Trie :
+    def __init__(self) :
+        self.root = TrieNode()
+
+    
+    def insert(self, word: str) -> None :
+        cur = self.root 
+
+        for c in word :
+            if c not in cur.children :
+                cur.children[c] = TrieNode()
+            cur = cur.children[c]
         
+        cur.endOfWord = True
+
+
+
+    def search(self, word : str) -> bool :
+        cur = self.root
+
+        for c in word :
+            if c not in cur.children :
+                return False
+            cur = cur.children[c]
+        return cur.endOfWord
+        
+
+    def startsWith(self, prefix : str) -> bool :
+        cur = self.root
+
+        for c in prefix :
+            if c not in cur.children :
+                return False
+            cur = cur.children[c]
+        return True
+
 
 
 # Your Trie object will be instantiated and called as such:
