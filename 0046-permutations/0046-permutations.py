e6d5c45ea2d733d0f103d0fd52c4c3b9f04c674e
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-15 13:45:31 +0900

    Time: 52 ms (19.77%), Space: 16.2 MB (98.06%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,21 +1,38 @@
+# class Solution:
+#     def permute(self, nums: List[int]) -> List[List[int]]:
+#         numLength = len(nums)
+#         if numLength == 1 :
+#             return [nums]
+#         result = []
+#         visited = [0 for _ in range(numLength)]
+#         def backtracking(perm, start_idx) :
+#             if len(perm) == numLength :
+#                 result.append(perm[:])
+#                 return
+
+#             for i in range(numLength) :
+#                 if not visited[i] :
+#                     visited[i] = 1
+#                     backtracking(perm + [nums[i]], i + 1)
+#                     visited[i] = 0
+
+#         backtracking([], 0)
+        
+#         return result
+
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
-        numLength = len(nums)
-        if numLength == 1 :
-            return [nums]
         result = []
-        visited = [0 for _ in range(numLength)]
-        def backtracking(perm, start_idx) :
-            if len(perm) == numLength :
-                result.append(perm[:])
-                return
+        if len(nums) == 1 :
+            return [nums[:]]
 
-            for i in range(numLength) :
-                if not visited[i] :
-                    visited[i] = 1
-                    backtracking(perm + [nums[i]], i + 1)
-                    visited[i] = 0
-
-        backtracking([], 0)
+        for i in range(len(nums)) :
+            n = nums.pop(0)
+            perms = self.permute(nums)
+            for perm in perms :
+                perm.append(n)
+            result.extend(perms)
+            nums.append(n)
         
         return result
+        
