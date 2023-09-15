16bfda131f6fd9e68218f2a0d7aef611dc78e939
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-15 13:13:24 +0900

    Time: 44 ms (64.77%), Space: 16.6 MB (35.57%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,7 +1,8 @@
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
         numLength = len(nums)
-
+        if numLength == 1 :
+            return [nums]
         result = []
         visited = [0 for _ in range(numLength)]
         def backtracking(perm, start_idx) :
