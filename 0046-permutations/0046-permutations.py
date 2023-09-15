972ddd74d47a01fbd02df2df55f300ed220ac9f0
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-15 17:14:21 +0900

    Time: 35 ms (97%), Space: 16.5 MB (35.57%) - LeetHub

--- 0046-permutations/0046-permutations.py
+++ 0046-permutations/0046-permutations.py
@@ -1,24 +1,24 @@
 # class Solution:
-#     def permute(self, nums: List[int]) -> List[List[int]]:
-#         numLength = len(nums)
-#         if numLength == 1 :
-#             return [nums]
-#         result = []
-#         visited = [0 for _ in range(numLength)]
-#         def backtracking(perm, start_idx) :
-#             if len(perm) == numLength :
-#                 result.append(perm[:])
-#                 return
+    # def permute(self, nums: List[int]) -> List[List[int]]:
+    #     numLength = len(nums)
+    #     if numLength == 1 :
+    #         return [nums]
+    #     result = []
+    #     visited = [0 for _ in range(numLength)]
+    #     def backtracking(perm, start_idx) :
+    #         if len(perm) == numLength :
+    #             result.append(perm[:])
+    #             return
 
-#             for i in range(numLength) :
-#                 if not visited[i] :
-#                     visited[i] = 1
-#                     backtracking(perm + [nums[i]], i + 1)
-#                     visited[i] = 0
+    #         for i in range(numLength) :
+    #             if not visited[i] :
+    #                 visited[i] = 1
+    #                 backtracking(perm + [nums[i]], i + 1)
+    #                 visited[i] = 0
 
-#         backtracking([], 0)
+    #     backtracking([], 0)
         
-#         return result
+    #     return result
 
 class Solution:
     def permute(self, nums: List[int]) -> List[List[int]]:
@@ -28,10 +28,12 @@ class Solution:
 
         for i in range(len(nums)) :
             n = nums.pop(0)
+
             perms = self.permute(nums)
             for perm in perms :
                 perm.append(n)
             result.extend(perms)
+            
             nums.append(n)
         
         return result
