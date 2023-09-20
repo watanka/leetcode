b4a054557d8f0d3d59e0d39cdcf62bb27dba2ff4
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-20 15:28:10 +0900

    Time: 54 ms (96.6%), Space: 17.5 MB (56.35%) - LeetHub

--- 0001-two-sum/0001-two-sum.py
+++ 0001-two-sum/0001-two-sum.py
@@ -1,9 +1,29 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
-        for l in range(len(nums) - 1) :
-            left = nums[l]
-            for r in range(l+1, len(nums)) :
-                right = nums[r]
+        num_dict = {}
+        for i, num in enumerate(nums) :
+            if num not in num_dict :
+                num_dict[num] = i
+            else :
+                # only one valid answer exists means there are maximum 2 same numbers.
+                num_dict[num] = [num_dict[num], i]
 
-                if left + right == target :
-                    return [l, r]
+        nums.sort()
+        l, r = 0, len(nums) - 1
+
+
+        while l < r :
+            currSum = nums[l] + nums[r]
+            if currSum == target :
+                if nums[l] == nums[r] :
+                    return num_dict[nums[l]]
+                else :
+                    return num_dict[nums[l]], num_dict[nums[r]]
+            elif currSum < target :
+                l += 1
+            else : 
+                r -= 1
+
+        return 
+
+        
