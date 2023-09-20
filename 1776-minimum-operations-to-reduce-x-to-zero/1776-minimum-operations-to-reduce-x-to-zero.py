752cda6c014a78c6c2a94fdda140d5036f5779ef
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-20 14:28:12 +0900

    Time: 948 ms (80.65%), Space: 30.2 MB (98.66%) - LeetHub

--- 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
+++ 1776-minimum-operations-to-reduce-x-to-zero/1776-minimum-operations-to-reduce-x-to-zero.py
@@ -1,31 +1,5 @@
 class Solution:
     def minOperations(self, nums: List[int], x: int) -> int:
-        # totalSum = sum(nums)
-        # target = totalSum - targetSum
-
-        # if target < 0 :
-        #     return -1
-        # if target == 0 :
-        #     return len(nums)
-
-        # n = len(nums)
-        # minOp = float('inf')
-        # currentSum = 0
-        # leftIndex = 0
-        # rightIndex = 0
-
-        # while rightIndex < n :
-        #     currentSum += nums[rightIndex]
-        #     rightIndex += 1
-
-        #     while currentSum > target and leftIndex < n :
-        #         currentSum -= nums[leftIndex]
-        #         leftIndex += 1
-
-        #     if currentSum == target :
-        #         minOps = min(minOps, n - (rightIndex - leftIndex))
-
-        # return -1 if minOps == float('inf') else minOps
 
         target = sum(nums) - x
         currSum = 0
