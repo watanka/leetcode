9290b75da2672eac6c35dd3b5591154bd4ce6165
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-21 16:37:32 +0900

    Time: 48 ms (76.78%), Space: 16.1 MB (95.3%) - LeetHub

--- 0013-roman-to-integer/0013-roman-to-integer.py
+++ 0013-roman-to-integer/0013-roman-to-integer.py
@@ -1,7 +1,8 @@
 class Solution:
     def romanToInt(self, s: str) -> int :
-        
-        symbol_dict = {
+        'III', 'LVIII', 'MCMXCIV'
+
+        letter_dict = {
             'I' : 1,
             'V' : 5,
             'X' : 10,
@@ -10,14 +11,31 @@ class Solution:
             'D' : 500,
             'M' : 1000
         }
-        total = 0
-        for i in range(len(s)) :
-            if i+1 < len(s) and symbol_dict[s[i]] < symbol_dict[s[i+1]] : # num_now and num_next is a combination num_now - num_next
-                total -= symbol_dict[s[i]]
-            else :
-                total += symbol_dict[s[i]]
-        
-        return total
 
-        
-            
+        result = 0
+        s = s.replace('IV', '!')
+        s = s.replace('IX', '@')
+        s = s.replace('XL', '#')
+        s = s.replace('XC', '$')
+        s = s.replace('CD', '%')
+        s = s.replace('CM', '^')
+
+        for letter in s :
+            if letter.isalpha() :
+                # for 0 1 2 3 5 6 7 8
+                result += letter_dict[letter]
+
+            else :
+                if letter == '!' :
+                    result += 4
+                elif letter == '@' :
+                    result += 9
+                elif letter == '#' :
+                    result += 40
+                elif letter == '$' :
+                    result += 90
+                elif letter == '%' :
+                    result += 400
+                elif letter == '^' :
+                    result += 900
+        return result
