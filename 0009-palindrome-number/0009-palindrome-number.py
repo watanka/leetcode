3306a52c112deebc9a71c6cbdefa1dc5f6d1aa9b
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-21 16:08:19 +0900

    Time: 237 ms (5.14%), Space: 16.4 MB (31.2%) - LeetHub

--- 0009-palindrome-number/0009-palindrome-number.py
+++ 0009-palindrome-number/0009-palindrome-number.py
@@ -1,7 +1,39 @@
 class Solution:
     def isPalindrome(self, x: int) -> bool:
-        x = str(x)
-        
-        return x[::-1] == x
+        if x < 0 :
+            return False
+        x = abs(x)
+        val = x
 
+        length = 1
+        num_ls = []
+        while True :
+            remainder = val % 10
+            num_ls.append(remainder)
+            val //= 10
+            
+            if val > 0 :
+                length += 1
+            else :
+                break
+        length = len(num_ls)
+        print(length)
+        for idx in range(length // 2) :
+            # 양쪽 끝에서부터 빼기
+            print('---')
+            print(idx)
+            leftval = x // (10 ** (length - 1 - idx))
+            rightval = (x % (10 ** (idx+1))) // (10 ** idx)
+            print(leftval)
+            print(rightval )
+        
+            if leftval != rightval :
+                return False
+            
+            x -= leftval * (10 ** (length - 1- idx))
+            print('after minus leftval', x)
+            x -= rightval * (10 ** idx)
+            print('after minus rightval', x)
+        
+        return True
         
