35e7d7b8b5c5e14e616e4a26453fb50950ac7fc1
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-22 22:25:00 +0900

    Time: 80 ms (83.47%), Space: 18 MB (47.94%) - LeetHub

--- 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
+++ 0026-remove-duplicates-from-sorted-array/0026-remove-duplicates-from-sorted-array.py
@@ -11,21 +11,5 @@ class Solution:
 
             i += 1
         
-        # change_idx = k 
-        # while change_idx < len(nums) :
-        #     nums[change_idx] = '_'
-        #     change_idx += 1
-
         return k
             
-
-    
-        # count = 1
-        # for i in range(1, len(nums)):
-        #   if nums[i] != nums[i-1]:
-        #     nums[count] = nums[i]
-        #     count += 1
-        # arr = [int(x) for x in set(nums)]
-        # print(len(arr))
-        # print(count)
-        return len(arr)
