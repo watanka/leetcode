9c50d996d0bde7098fbb740a254e1bfde15421fc
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-23 23:20:22 +0900

    Time: 40 ms (47.71%), Space: 16.2 MB (88.43%) - LeetHub

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -11,18 +11,14 @@ class Solution:
         #         needle_idx = 0
         # return -1
 
-        
+        if len(haystack) == len(needle) :
+            if haystack == needle :
+                return 0
+            else :
+                return -1
+            
 
         for i in range(len(haystack) - len(needle) + 1) :
-            start_idx = 0
-            while start_idx < len(needle) :
-                if haystack[i + start_idx] == needle[start_idx] :
-                    start_idx += 1
-                else :
-                    # 한글자라도 다를 경우 종료
-                    break
-            # 매칭 확인
-            if start_idx == len(needle) :
+            if haystack[i : i + len(needle)] == needle :
                 return i
-
         return -1
