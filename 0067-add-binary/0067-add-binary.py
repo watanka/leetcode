2e9022d2f067db4d10f745ced2a6edee3cec15b2
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-25 15:03:00 +0900

    Time: 34 ms (91.64%), Space: 16.2 MB (70.5%) - LeetHub

--- 0067-add-binary/0067-add-binary.py
+++ 0067-add-binary/0067-add-binary.py
@@ -12,32 +12,34 @@ class Solution:
         # for idx in range( length - 1, -1, -1 ))
         # up, val = binsum(letter_a, letter_b). up = 1 if letter_a = 1, letter_b = 1
 
-        def binsum(letterA : int, letterB : int, up : int) -> tuple[str, str] :
-            sumVal = letterA + letterB + up
-            if sumVal == 1 :
-                return '1', '0' # val, up
-            elif sumVal == 2 :
-                return '0', '1'
-            elif sumVal == 3 :
-                return '1', '1'
-            elif sumVal == 0: 
-                return '0', '0'
+        # def binsum(letterA : int, letterB : int, up : int) -> tuple[str, str] :
+        #     sumVal = letterA + letterB + up
+        #     if sumVal == 1 :
+        #         return '1', '0' # val, up
+        #     elif sumVal == 2 :
+        #         return '0', '1'
+        #     elif sumVal == 3 :
+        #         return '1', '1'
+        #     elif sumVal == 0: 
+        #         return '0', '0'
 
         
 
-        length = max(len(a), len(b))
-        # a = '100', b = '1' => a = '100', b = '001'
-        if length - len(a) :
-            a = '0' * (length - len(a)) + a 
-        else :
-            b = '0' * (length - len(b)) + b
+        # length = max(len(a), len(b))
+        # # a = '100', b = '1' => a = '100', b = '001'
+        # if length - len(a) :
+        #     a = '0' * (length - len(a)) + a 
+        # else :
+        #     b = '0' * (length - len(b)) + b
 
-        result = ''
-        up = '0'
-        for idx in range(length - 1, -1, -1 ) :
-            val, up = binsum(int(a[idx]), int(b[idx]), int(up))
-            result = val + result
-        if up == '1' : 
-            result = up + result
+        # result = ''
+        # up = '0'
+        # for idx in range(length - 1, -1, -1 ) :
+        #     val, up = binsum(int(a[idx]), int(b[idx]), int(up))
+        #     result = val + result
+        # if up == '1' : 
+        #     result = up + result
 
-        return result
+        # return result
+
+        return bin(int(a, base = 2) + int(b, base = 2))[2:]
