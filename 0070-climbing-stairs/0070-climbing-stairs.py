a4391f9236b6694c2c3a56044dcf1dd1cca0428f
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-09-25 16:24:20 +0900

    Time: 41 ms (31.16%), Space: 16.2 MB (58.87%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.py
+++ 0070-climbing-stairs/0070-climbing-stairs.py
@@ -25,7 +25,11 @@ class Solution:
         dp[0] = 0
         dp[1] = 1
         dp[2] = 2
-        for i in range(3, 46) :
+
+        if n < 3 :
+            return n
+    
+        for i in range(3, n+1) :
             dp[i] = dp[i - 1] + dp[i - 2]
             
 
