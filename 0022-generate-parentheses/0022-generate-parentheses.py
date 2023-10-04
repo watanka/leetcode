a24c5d4e4b8ebd4d905c2836ea2c034fe445b9aa
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-10-04 13:29:34 +0900

    Time: 50 ms (11.97%), Space: 16.6 MB (77.89%) - LeetHub

---
+++ 0022-generate-parentheses/0022-generate-parentheses.py
@@ -0,0 +1,20 @@
+class Solution:
+    def generateParenthesis(self, n: int) -> List[str]:
+        result = []
+
+        def backtracking(n_open, n_close, comb) : 
+            if n_open == n_close == n :
+                if comb not in result :
+                    result.append(comb)
+                    return
+
+
+            for pthsis in [0, 1] : # put write ( or ).[open, close]
+                if pthsis == 0 and n_open < n :
+                    backtracking(n_open + 1, n_close, comb + '(')
+                elif pthsis == 1 and n_close < n_open :
+                    backtracking(n_open, n_close + 1, comb + ')')
+
+        backtracking(0, 0, '')
+
+        return result
