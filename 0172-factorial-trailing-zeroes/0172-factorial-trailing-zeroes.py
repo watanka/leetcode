946f9bb9507c2c5186e23998a35aca2d9ccca3d9
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-10-17 13:43:26 +0900

    Time: 46 ms (44.17%), Space: 16.2 MB (77.61%) - LeetHub

---
+++ 0172-factorial-trailing-zeroes/0172-factorial-trailing-zeroes.py
@@ -0,0 +1,24 @@
+class Solution:
+    def trailingZeroes(self, n: int) -> int:
+        '''
+        n = 0, 0! = 0         1
+        n = 1, 1! = 1         0
+        n = 2, 2! = 2         0
+        n = 3, 3! = 6         0
+        n = 4, 4! = 24        0
+        n = 5, 5! = 120       1
+        n = 6, 6! = 720       1
+        n = 7, 7! = 5040      1
+        n = 8, 8! = 40320     1
+        n = 9, 9! = 362880    1
+        n = 10, 10! = 3628800 2
+        how many 10 in the value => there are enough of 2 compared to 5. => how many 5s?
+        '''
+        a = 1
+        b = 5 ** a
+        c = 0
+        while b <= n :
+            c += n // b
+            a += 1
+            b = 5 ** a
+        return c
