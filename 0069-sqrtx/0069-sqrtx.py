4a411ec6eb99fc68b703e1a93ab21f79eaf42b41
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-10-22 23:08:06 +0900

    Time: 48 ms (44.34%), Space: 16.2 MB (39.71%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -16,12 +16,32 @@ class Solution:
         # 3 = {9, 10, 11, 12, 13, 14, 15} = #7
         # 4 = {16, 17, 18, 19, 20, 21, 22, 23, 24} = #9
 
+        # 3 + 5 + 7 + 9 = 3 * 4 + 2 * 3 + 2 * 2 + 2
 
-        sqrt = 0
-        sub = 3
-        while x > 0:
-            x -= sub
-            sub += 2
-            sqrt += 1
+        # sqrt = 0
+        # sub = 3
+        # while x > 0:
+        #     x -= sub
+        #     sub += 2
+        #     sqrt += 1
         
-        return sqrt
+        # return sqrt
+
+
+        left, right = 0, x
+
+
+        while left <= right :
+
+            mid = (left + right) // 2
+            if mid ** 2 == x :
+                return mid
+            elif mid ** 2 > x :
+                right = mid - 1
+            else :
+                left = mid + 1
+
+        return left-1
+            
+            
+
