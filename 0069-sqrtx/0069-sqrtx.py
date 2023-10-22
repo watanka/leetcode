4362b3704e31f913c32ad5cf98d25b368a687223
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2023-10-22 22:50:10 +0900

    Time: 1866 ms (10.01%), Space: 16.3 MB (9.84%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.py
+++ 0069-sqrtx/0069-sqrtx.py
@@ -1,3 +1,27 @@
 class Solution:
     def mySqrt(self, x: int) -> int:
-        return int(math.sqrt(x))
+
+        # 1 = 1
+        # 2 = 4
+        # 3 = 9
+        # 4 = 16
+        # 5 = 25
+        # 6 = 36
+        # 7 = 49
+        # 8 = 64
+
+
+        # 1 = {1, 2, 3} = #3 
+        # 2 = {4, 5, 6, 7, 8} = #5
+        # 3 = {9, 10, 11, 12, 13, 14, 15} = #7
+        # 4 = {16, 17, 18, 19, 20, 21, 22, 23, 24} = #9
+
+
+        sqrt = 0
+        sub = 3
+        while x > 0:
+            x -= sub
+            sub += 2
+            sqrt += 1
+        
+        return sqrt
