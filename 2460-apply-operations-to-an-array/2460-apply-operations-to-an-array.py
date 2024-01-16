1dd1d51015ae354f29651415cc36c4734be58e42
Eunsung Shin <38347743+watanka@users.noreply.github.com>
2024-01-16 11:39:01 +0900

    Time: 56 ms (48.77%), Space: 17.6 MB (41.23%) - LeetHub

--- 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
+++ 2460-apply-operations-to-an-array/2460-apply-operations-to-an-array.py
@@ -6,6 +6,18 @@ class Solution:
                 nums[i] *= 2
                 nums[i+1] = 0
         
+        curr, prev = 0, 0
+
+        while curr < len(nums) :
+            if nums[curr] != 0 :
+                nums[prev], nums[curr] = nums[curr], nums[prev]
+                prev += 1
+                curr += 1
+
+            else :
+                curr += 1
+
+        return nums
         # 0을 오른쪽으로 다 옮기기
         # for left in range(len(nums)) :
         #     if nums[left] != 0 :
@@ -14,23 +26,17 @@ class Solution:
         #         if nums[right] != 0 :
         #             nums[right], nums[left] = nums[left], nums[right]
         #             break
-        zero_cnt = 0
-        new_list = []
-        for i in range(len(nums)) :
-            if nums[i] != 0 :
-                new_list.append(nums[i])        
-            else :
-                zero_cnt += 1
+        # zero_cnt = 0
+        # new_list = []
+        # for i in range(len(nums)) :
+        #     if nums[i] != 0 :
+        #         new_list.append(nums[i])        
+        #     else :
+        #         zero_cnt += 1
             
-        return new_list + [0] * zero_cnt
+        # return new_list + [0] * zero_cnt
                 
 
-            
-
-        return nums
-                
-
-                
 
 
         
